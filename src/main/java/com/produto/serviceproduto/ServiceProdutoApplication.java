02248614109f5271bc67c5b3de379dfe24335efe
willianjoga6 <60036882+willianjoga6@users.noreply.github.com>
2024-10-01 18:25:26 -0300

    Commit inicial

---
+++ src/main/java/com/produto/serviceproduto/ServiceProdutoApplication.java
@@ -0,0 +1,16 @@
+package com.produto.serviceproduto;
+
+import jakarta.persistence.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import javax.persistence.*;
+
+@SpringBootApplication
+public class ServiceProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProdutoApplication.class, args);
+    }
+
+}
